0eb782afd3a979e699000b6faec2d22b86b09a14
Fao, Sean <sean.fao@gmail.com>
2026-07-20 21:30:40 -0600

    Started creating a map based on some sprites I bought.

---
+++ maps/Tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Tilesheet" tilewidth="16" tileheight="16" tilecount="260" columns="13">
+ <image source="../sprites/Tilesheet.png" width="208" height="320"/>
+</tileset>
